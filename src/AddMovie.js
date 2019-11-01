a9e779091e7ffa5ebab970cb9f171969b2667106
beata <beata@emc2wave.com>
2019-10-31 21:39:30 -0700

    add AddMovie file

---
+++ src/AddMovie.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+
+const AddMovie = () => {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+
+  const updateName = (e) => {
+    setName(e.target.value);
+  }
+
+  const updatePrice = (e) => {
+    setPrice(e.target.value);
+  }
+
+  addMovie = e => {
+    e.preventDefault();
+  }
+
+  return (
+    <form>
+      <input type="text" name="name" value={name} onChange={updateName}/>
+      <input type="text" price="price" value={price} onChange={updatePrice}/>
+      <button>Submit</button>
+    </form>
+  )
+}
+
+
